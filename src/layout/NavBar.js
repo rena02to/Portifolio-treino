729abd4023c50a9e456c1c87665bb28e3f49b917
rena02to <rena02to@gmail.com>
2023-10-07 22:38:47 -0300

    Formatando a Barra de Navegação e a Home do site.

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -12,13 +12,13 @@ function NavBar(){
     return(
         <div>
             <nav className={style.navBar}>
-                <a href='#home' onClick={handleLinkClick}><h1 className={style.logo}>&lt; RA /&gt;</h1></a>
+                <a href='#home' onClick={handleLinkClick}><h1 className={style.logo}>&lt;RA/&gt;</h1></a>
                 <ul className={style.elementos}>
-                    <li><a href='#home' onClick={handleLinkClick}>Home</a></li>
-                    <li><a href='#projetos' onClick={handleLinkClick}>Projetos</a></li>
-                    <li><a href='#experiencias' onClick={handleLinkClick}>Experiências</a></li>
-                    <li><a href='#sobre' onClick={handleLinkClick}>Sobre</a></li>
-                    <li><button href='#contato' onClick={handleLinkClick}>Contate-me</button></li>
+                    <li><a className={style.home} href='#home' onClick={handleLinkClick}><p>Home</p></a></li>
+                    <li><a className={style.projetos} href='#projetos' onClick={handleLinkClick}><p>Projetos</p></a></li>
+                    <li><a className={style.experiencias} href='#experiencias' onClick={handleLinkClick}><p>Experiências</p></a></li>
+                    <li><a className={style.sobre} href='#sobre' onClick={handleLinkClick}><p>Sobre</p></a></li>
+                    <li><button className={style.contate} href='#contato' onClick={handleLinkClick}><p>Contate-me</p></button></li>
                 </ul>
             </nav>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
+import dev from './../imagens/11436094_4707119.svg';
+import style from './Home.module.css';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { TbBrandGithubFilled } from 'react-icons/tb';
+
 function Home(){
     return(
-        <div id="home">
-            <h1>Home</h1>
+        <div id="home" className={style.home}>
+            <div className={style.texto}>
+                <h2 className={style.ola}>Olá, eu sou</h2>
+                <h1 className={style.name}>Renato Alves,</h1>
+                <h2 className={style.dev}>Desenvolvedor front-end</h2>
+                <div className={style.botoes}>
+                    <a href='https://linkedin.com/in/renato-alves-2284a6236' target='_blank' rel='noopener noreferrer'>
+                        <div className={style.linkedin}>
+                            <FaLinkedinIn />
+                            <p>Linkedin</p>
+                        </div>
+                    </a>
+                    <a href='https://github.com/rena02to' target='_blank' rel='noopener noreferrer'>
+                        <div className={style.github}>
+                            <TbBrandGithubFilled />
+                            <p>Github</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <img alt='dev' src={dev} />
         </div>
     );
 }
