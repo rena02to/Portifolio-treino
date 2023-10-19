ea14a7fafdd71c623933970358620c20d6b22d34
rena02to <rena02to@gmail.com>
2023-10-19 18:05:25 -0300

    Adicionando meu portifólio principal na aba projetos.

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,5 +1,6 @@
 import carnivorusPlaint from './../imagens/carnivorous-plant.png';
 import style from './Projetos.module.css';
+import { PiLinkLight } from 'react-icons/pi';
 
 function Projetos(){
     return(
@@ -8,6 +9,23 @@ function Projetos(){
                 <img alt='carnivorus plaint' className={style.carnivorusPlaint} src={carnivorusPlaint} />
                 <h1>Projetos</h1>
                 <img alt='carnivorus plaint' className={style.carnivorusPlaint} src={carnivorusPlaint} />
+            </div>
+
+            <div className={style.portifolio}>
+                <div className={style.portifolioConteudo}>
+                    <spam className={style.interno}>
+                        <p className={style.legenda}>Portifólio principal</p>
+                        <a href='https://renato-alves.vercel.app' target='_blank' rel='noopener noreferrer'>
+                            <div>
+                                <PiLinkLight />
+                                <p>Conheça o projeto clicando aqui</p>
+                            </div>
+                        </a>
+                    </spam>
+                </div>
+                <h2 className={style.titleProject}>Portifólio Principal</h2>
+                <p className={style.legendProject}>Meu futuro portifólio principal que será criado para divulgar todos os meus trabalhos e projetos, de forma que estará em constante evolução e desenvolvimento.</p>
+                <p className={style.legend2Project}>Portanto, se quiser acompanhar todos os meus trabalhos, fique de olho nesse portifólio.</p>
             </div>
         </div>
     );
